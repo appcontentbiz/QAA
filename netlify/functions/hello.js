5ac5433ad7261be6e5e32bea99b55b8732038382
Your Name <you@example.com>
2025-03-02 01:41:58 -0600

    Add CORS headers to function

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,4 +1,16 @@
 exports.handler = async function(event, context) {
+  // Handle preflight requests
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+      }
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
